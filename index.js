afebac28e9f60741ee1e790346421223811f3963
Miloloaf <Miloloaf@users.noreply.github.com>
2020-09-27 08:41:00 +0100

    Second Image Text

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
-
-
-function firstImageDown() {
-    document.getElementById("grid-cell-1-description").style.opacity = "1";
-
+function firstImageDown(e) {
+  document.getElementById('grid-cell-1-description').style.opacity = '1'
 }
 
-function firstImageUp() {
-    document.getElementById("grid-cell-1-description").style.opacity = "0";
+function firstImageUp(e) {
+  document.getElementById('grid-cell-1-description').style.opacity = '0'
 }
 
-document.getElementById("grid-cell-1-description").addEventListener("mousedown", firstImageDown);
-document.getElementById("grid-cell-1-description").addEventListener("mouseup", firstImageUp);
-document.getElementById("grid-cell-1-description").addEventListener("touchstart", firstImageDown);
-document.getElementById("grid-cell-1-description").addEventListener("touchend", firstImageUp);
+function secondImageDown() {
+    document.getElementById('grid-cell-2-description').style.opacity = '1'
+}
+
+function secondImageUp() {
+document.getElementById('grid-cell-2-description').style.opacity = '0'
+}
+
+document
+  .getElementById('grid-cell-1-description')
+  .addEventListener('mousedown', firstImageDown)
+document
+  .getElementById('grid-cell-1-description')
+  .addEventListener('mouseup', firstImageUp)
+document
+  .getElementById('grid-cell-1-description')
+  .addEventListener('touchstart', firstImageDown)
+document
+  .getElementById('grid-cell-1-description')
+  .addEventListener('touchend', firstImageUp)
+
+function addListeners(idToSearch, ondown, onup) {
+    
+
+  document.getElementById(idToSearch).addEventListener('mousedown', ondown);
+  document.getElementById(idToSearch).addEventListener('mouseup', onup);
+  document.getElementById(idToSearch).addEventListener('touchstart', ondown)
+  document.getElementById(idToSearch).addEventListener('touchend', onup)
+}
+
+addListeners('grid-cell-2-description', secondImageDown, secondImageUp)
